83e2ffa4d3bac2ba7aee35a920341147f92ac35b
akinyiliz <odhiamboeliza84@gmail.com>
2024-10-30 18:35:35 +0300

    chore: Config app custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      mainBg: '#f7e7ff',
+      cardBg: '#090412',
+      inputBg: '#26183c',
+      whiteText: '#e9e8ea',
+      grayText: '#56505d',
+      secondary: '#d181ff',
+      icon: '#814e9e',
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
