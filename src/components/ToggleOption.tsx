da20633f09b54bbae71a19ac704cdbd9f4025871
akinyiliz <odhiamboeliza84@gmail.com>
2024-10-31 17:07:44 +0300

    feat: Create reusable setting tab

---
+++ src/components/ToggleOption.tsx
@@ -0,0 +1,28 @@
+import { BsToggleOff, BsToggleOn } from 'react-icons/bs';
+
+type ToggleOptionProps = {
+  label: string;
+  value: boolean;
+  onValueChange: (value: boolean) => void;
+};
+
+const ToggleOption = ({ label, value, onValueChange }: ToggleOptionProps) => {
+  return (
+    <div className="settings-tab">
+      <label htmlFor="uppercaseAllowed" className="text-sm md:text-base">
+        {label}
+      </label>
+      {value ? (
+        <BsToggleOn
+          size={33}
+          color="#d181ff"
+          onClick={() => onValueChange(false)}
+        />
+      ) : (
+        <BsToggleOff size={33} onClick={() => onValueChange(true)} />
+      )}
+    </div>
+  );
+};
+
+export default ToggleOption;
